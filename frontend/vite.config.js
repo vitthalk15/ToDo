2a8ac1548e3d0cdcd69283402171f56d913a3b43
Vitthal <vitthal@kali.kali>
2025-04-28 14:35:01 +0530

    added backenfd url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://todo-abep.onrender.com',
       },
     },
   },
